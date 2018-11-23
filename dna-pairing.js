c25e254a88e5a4f83d7ffa10cc713a805cfa826a
Troy Jennings <troy.c.jennings@gmail.com>
2018-11-23 14:01:59 +0100

    final dna-pairing solution

---
+++ dna-pairing.js
@@ -0,0 +1,32 @@
+/**
+ * Return the DNA strand with its missing pairing element.
+ * @param {str} str The the DNA strand to convert.
+ * @return {str} The converted DNA strand.
+ */
+
+ // Skills Demonstrated
+ // * Array mapping
+
+// My Solution
+function pairElement(str) {
+    let dnaStrand = str.split('');
+    let mappedPairs = [];
+
+    // our base pairs for matching
+    const basePairs = {
+        "A": "T", "T": "A", "C": "G", "G": "C"
+    };
+
+    dnaStrand.forEach((base, index) => {
+        let dnaObj = [];
+        dnaObj.push(base, basePairs[base]);
+        mappedPairs[index] = dnaObj;
+    });
+
+    return mappedPairs;
+}
+
+// Tests
+console.log(pairElement("ATCGA")); // should return [["A", "T"], ["T", "A"], ["C", "G"], ["G", "C"], ["A", "T"]].
+console.log(pairElement("TTGAG")); // should return [["T", "A"], ["T", "A"], ["G", "C"], ["A", "T"], ["G", "C"]].
+console.log(pairElement("CTCTA")); // should return [["C", "G"], ["T", "A"], ["C", "G"], ["T", "A"], ["A", "T"]].
