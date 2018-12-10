8dab0dd291d662664f2ebd708afe8947622172ec
Troy Jennings <troy.c.jennings@gmail.com>
2018-12-10 20:44:32 +0100

    refactored solution

--- dna-pairing.js
+++ dna-pairing.js
@@ -32,7 +32,7 @@ const pairElement = (str) => {
     return str.split('').map(letter => [letter, basePairs[letter]])
 }
 
-// Tests
+// TESTS
 console.log(pairElement("ATCGA")); // should return [["A", "T"], ["T", "A"], ["C", "G"], ["G", "C"], ["A", "T"]].
 console.log(pairElement("TTGAG")); // should return [["T", "A"], ["T", "A"], ["G", "C"], ["A", "T"], ["G", "C"]].
 console.log(pairElement("CTCTA")); // should return [["C", "G"], ["T", "A"], ["C", "G"], ["T", "A"], ["A", "T"]].
